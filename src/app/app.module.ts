c97a274a7deedc4e2396f13822e5a85fff11f1bf
transachthien <thientshe141579@fpt.edu.vn>
2024-05-15 10:40:50 +0700

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DefaultLayoutModule } from './core/default-layout/default-layout.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { SharedAppModule } from './core/shared/shared.module';
+import { RegisterComponent } from './modules/auth/register/register.component';
+import { ResetPasswordComponent } from './modules/auth/reset-password/reset-password.component';
+import { FeatureGuard } from './core/permission/guards/feature.guard';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './core/shared/interceptors/auth.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './modules/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetPasswordComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    DefaultLayoutModule,
+    DashboardModule,
+    SharedAppModule,
+    BrowserAnimationsModule,
+    NgbModule
+
+  ],
+  providers: [
+    FeatureGuard,
+    // {
+    //   provide: LocationStrategy,
+    //   useClass: HashLocationStrategy
+    // },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
