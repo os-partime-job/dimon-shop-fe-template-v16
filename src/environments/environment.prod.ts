94c15ccf97e785a438f0986a4e0f129f8e6abf40
Thành Đặng <85785895+thanhdev-work@users.noreply.github.com>
2024-05-27 22:56:00 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   api_url:'http://localhost:3000/api/v1/',
   ws_url:'http://localhost:3000',
-  companyId:'1254863933218p0ppp'
+  companyId:'1254863933218p0ppp',
+  apiUrl:'http://178.128.111.191:8091',
+  pageApi:'http://localhost:3000',
 };
