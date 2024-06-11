e57f726e636e63f5d8b9bfe3ccdbff651a14d881
transachthien <transachthien@gmail.com>
2024-06-11 23:21:25 +0700

    commit product list

--- src/app/modules/pages/product-list/product-list.component.ts
+++ src/app/modules/pages/product-list/product-list.component.ts
@@ -1,10 +1,122 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {ProductService} from "../../service/product.service";
+import {ToastrService} from "ngx-toastr";
+import {NumberService} from "../../service/number.service";
+import {AccountService} from "../../auth/services/account.service";
+import {AuthGoogleService} from "../../../core/shared/auth-google.service";
+import {CartService} from "../../service/cart.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+  selectTypePrice :string = '';
+  selectOptionType :string = '';
+  searchProduct: string = '';
+  products: any[];
+  isLoginUser:boolean = false;
+  optionSelectPrices = [
+    {
+      name:"Price 0->50 millions VND",
+      id:1
+    },
+    {
+      name:"Price 50->100 millions VND",
+      id:2
+    },
+    {
+      name:"Price upper 100 millions VND",
+      id:3
+    },
+  ];
+  public jewelryTypes : any[] = [];
 
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService,
+              private toastrService: ToastrService,
+              private numberFormat: NumberService,
+              private accountServie: AccountService,
+              private googleService :AuthGoogleService,
+              private cartService: CartService,
+              private ProductService: ProductService,) {
+  }
+  ngOnInit(): void {
+    this.isLoginUser = localStorage.getItem("user") != null;
+    this.getJewelryTypes();
+    this.getProducts();
+  }
+
+  onChange(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    console.log(selectedValue);
+  }
+
+  removeFilterPrice() {
+    this.selectTypePrice = "";
+    console.log(this.selectTypePrice);
+  }
+  getProducts(){
+    let request = {
+      jewelry_type_id:this.selectOptionType,
+      title:this.searchProduct,
+      limit:1000,
+      offset:0,
+      requestId:''
+    }
+
+    this.productService.getProducts(request).subscribe((res) => {
+      this.products = res.data;
+    }, error => {
+      this.toastrService.error("Error get famous products");
+    });
+  }
+  convertNumber(number){
+    return this.numberFormat.convertNumber(number);
+  }
+
+  search() {
+    console.log("selectTypePrice: ",this.selectTypePrice, "selectOptionType", this.selectOptionType, "searchProduct", this.searchProduct)
+
+  }
+  private getJewelryTypes() {
+    this.ProductService.getJewelryType().subscribe((res) =>{
+      this.jewelryTypes = [...res.data];
+    },error => {
+      this.toastrService.error("Error get Type")
+    })
+
+  }
+  getProductCart() {
+    const request = {
+      customer_id : 1
+    }
+    this.cartService.getProductInCart(request).subscribe((res) =>{
+      this.cartService.cartItems.next(res?.data);
+      this.cartService.totalProductInCart.next(this.cartService.getTotalProduct(res?.data));
+      this.cartService.totalPrice.next(this.cartService.getTotalPriceV2(res?.data));
+    }, error => {
+
+    })
+  }
+  addProductCart(product:any){
+    if(!this.isLoginUser) {
+      this.toastrService.error("Bạn phải đăng nhập trước");
+      return;
+    }
+    const request = {
+      jewelry_id : product?.id_jewelry,
+      quantity : 1
+    }
+    this.cartService.addProductToCard(request).subscribe((res) =>{
+      this.toastrService.success("Add sản phầm thành công");
+      this.getProductCart();
+    }, error => {
+      this.toastrService.error("Add sản phầm thất bại");
+    });
+  }
 }
