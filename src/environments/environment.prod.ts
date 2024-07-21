ec40efc418de240c275621f4f0493a7b2b9e135e
transachthien <transachthien@gmail.com>
2024-07-21 08:35:53 +0700

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,10 @@ export const environment = {
   api_url:'http://localhost:3000/api/v1/',
   ws_url:'http://localhost:3000',
   companyId:'1254863933218p0ppp',
-  apiUrl:'http://178.128.111.191:8091',
+  // apiUrl: 'https://devapibhs.k-media.vn',
+  // apiUrl:'http://178.128.111.191:8091',
+  apiUrl: 'http://localhost:8080',
+  // apiPayment :'http://178.128.111.191:8081',
+  apiPayment :'http://localhost:8081',
   pageApi:'http://localhost:3000',
 };
